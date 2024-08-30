fd16a0c8cc54f1b507263f8c44b3ea07ffe960b3
Taha Ahmed <taha@mage4.com>
2024-08-30 23:43:36 +0500

    install

--- install.sh
+++ install.sh
@@ -88,8 +88,7 @@ sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
     https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
     /etc/apt/sources.list.d/jenkins.list >/dev/null
-sudo apt-get update
-sudo apt-get install jenkins -y
+sudo apt-get update && sudo apt-get install jenkins -y
 sudo systemctl start jenkins
 #sudo systemctl status jenkins
 
